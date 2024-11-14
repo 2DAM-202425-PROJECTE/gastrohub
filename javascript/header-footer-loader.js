36a29b50d972a1a3851109e77fac5351b000701a
Christian Villanueva <christianvillanueva@iesebre.com>
2024-11-14 16:24:52 +0100

    login llevat del index, fondo arreglat, cohet mig arreglat i nom al header

--- javascript/header-footer-loader.js
+++ javascript/header-footer-loader.js
@@ -17,6 +17,7 @@ function loadNavbarFooter() {
     <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div class="logo-head">
             <a href="/index.html"><img src="/assets/logoblack.png" alt="GastroHub"/></a>
+            <p>GASTROHUB</p>
         </div>
         <div class="w-[700px] h-full flex justify-evenly items-center hidden lg:flex lg:gap-x-12 text-pblack dark:text-pwhite font-bold">
             <a class="hover:text-pteal" href="/features.html">FEATURES</a>
