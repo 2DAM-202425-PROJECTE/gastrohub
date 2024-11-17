3e638b09d9bb1f2b02b3c397e748d7f5606c2f91
chhriistiaann <103261306+chhriistiaann@users.noreply.github.com>
2024-11-17 17:48:17 +0100

    aaa

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}", "./public/**/*.html",
+  content: [
+    "./**/*.{html,js}", 
+    "./public/*.html",
   ],
   theme: {
     extend: {
