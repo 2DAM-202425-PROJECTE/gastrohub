8fc60b16e0a8bea3ec6ea7e372a6c871ace6deb4
chhriistiaann <103261306+chhriistiaann@users.noreply.github.com>
2024-11-30 16:52:46 +0100

    Update header.js

--- public/javascript/header.js
+++ public/javascript/header.js
@@ -9,26 +9,26 @@ document.addEventListener('DOMContentLoaded', (event) => {
     }
 });
 
-const btn = document.getElementById('button');
+// const btn = document.getElementById('button');
 
-document.getElementById('contact').addEventListener('submit', function(event) {
-    event.preventDefault();
+// document.getElementById('contact').addEventListener('submit', function(event) {
+//     event.preventDefault();
 
-    document.getElementById("responseMessage").classList.add("hidden");
-    document.getElementById("errorMessage").classList.add("hidden");
+//     document.getElementById("responseMessage").classList.add("hidden");
+//     document.getElementById("errorMessage").classList.add("hidden");
 
-    const btn = document.getElementById('button');
-    btn.value = 'Sending...';
+//     const btn = document.getElementById('button');
+//     btn.value = 'Sending...';
 
-    const serviceID = 'service_ap02tii';
-    const templateID = 'template_qa363di';
+//     const serviceID = 'service_ap02tii';
+//     const templateID = 'template_qa363di';
 
-    emailjs.sendForm(serviceID, templateID, this).then(function(response) {
-        console.log("Success!", response.status, response.text);
-        document.getElementById("responseMessage").classList.remove("hidden");
-        document.getElementById("contactForm").reset();
-    }, function(error) {
-        console.error("Error:", error);
-        document.getElementById("errorMessage").classList.remove("hidden");
-    });
-});
+//     emailjs.sendForm(serviceID, templateID, this).then(function(response) {
+//         console.log("Success!", response.status, response.text);
+//         document.getElementById("responseMessage").classList.remove("hidden");
+//         document.getElementById("contactForm").reset();
+//     }, function(error) {
+//         console.error("Error:", error);
+//         document.getElementById("errorMessage").classList.remove("hidden");
+//     });
+// });
